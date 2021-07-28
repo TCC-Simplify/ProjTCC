f79220bdae78aef50ae0a156326add8c36c8d031
felipeakira-exe <79931386+felipeakira-exe@users.noreply.github.com>
2021-07-28 17:58:08 -0300

    Atualização página cadastro empresa

--- public/js/funcs_cad_empresa.js
+++ public/js/funcs_cad_empresa.js
@@ -1,18 +1,25 @@
 function mostrarSenha(){
 	var tipo = document.getElementById("senha");
+	var botao = document.getElementById("botao");
+	var botao2 = document.getElementById("botao2");
 	if(tipo.type == "password"){
 		tipo.type = "text";
+		botao.style.color = "red";
 	}else{
 		tipo.type = "password";
+		botao.style.color = "white";
 	}
+
 }
 
 function mostrarSenha2(){
 	var tipo = document.getElementById("senha2");
 	if(tipo.type == "password"){
 		tipo.type = "text";
+		botao2.style.color = "red";
 	}else{
 		tipo.type = "password";
+		botao2.style.color = "white";
 	}
 }
 
