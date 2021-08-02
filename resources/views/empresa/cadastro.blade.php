6c9a83da1888bf45e4d049c3b1ffc0f8b4e9e188
felipeakira-exe <feakinoz@gmail.com>
2021-08-02 09:35:03 -0300

    atualizacoes pagina welcome cad e login

--- resources/views/empresa/cadastro.blade.php
+++ resources/views/empresa/cadastro.blade.php
@@ -1,147 +1,43 @@
 <!DOCTYPE html>
 <html lang="en">
 
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>cadastrar</title>
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;600;800&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-        <link href="{{ asset('css/user_e_empresa/cadastro_e_login.css') }}" rel="stylesheet">
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous">
-        
-        <script src="https://code.jquery.com/jquery-3.0.0.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/@vsilva472/jquery-viacep/dist/jquery-viacep.min.js"></script>
-        <script src="<?php echo asset('js/jquery.maskedinput-1.1.4.pack.js')?>" type="text/javascript"></script>
-        <script src="<?php echo asset('js/funcs_cad_empresa.js')?>"></script> 
-        <style>
-            * {
-            font-weight: 100;
-            margin: 0;
-            border: 0;
-            padding: 0;
-            box-sizing: border-box;
-            font-family: 'Poppins', sans-serif;
-            color: #fff;
-        }
-        body {
-            background: linear-gradient(90deg, #edf2f4, #4a00e0);
-            background-repeat: no-repeat;
-            min-height: 100vh;
-            min-width: 100vw;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-        }
-        main.container {
-            background: #212529;
-            width: 50vw;
-            min-height: 70vh;
-            padding: 2rem;
-            box-shadow: 5px 5px 15px rgba(0, 0, 0, 0.2);
-            border-radius: 20px;
-            margin: 1rem;
-        }
-        #header {
-            display: flex;
-        }
-        #voltar {
-            margin-left: auto;
-        }
-        #voltar a{
-            text-decoration: none;
-            font-size: 17px;
-            color: #edf2f4;
-            padding: 1rem;
-        }
-        #voltar a:hover {
-            color: #e0aaff;
-        }
-        .label-float{
-            position: relative;
-            padding-top: 13px;
-        }
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>cadastrar</title>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;600;800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <!--<link href="{{ asset('css/user_e_empresa/cadastro_e_login.css') }}" rel="stylesheet">-->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous">
+    
+    <script src="https://code.jquery.com/jquery-3.0.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@vsilva472/jquery-viacep/dist/jquery-viacep.min.js"></script>
+    <script src="<?php echo asset('js/jquery.maskedinput-1.1.4.pack.js')?>" type="text/javascript"></script>
+    <script src="<?php echo asset('js/funcs_cad_empresa.js')?>"></script> 
+    
+    <link rel="stylesheet" href="{{ asset('css/empresa/cadastro.empresa.css') }}">
+    
+</head>
 
-        .label-float input{
-            border: 1px solid lightgrey;
-            border-radius: 5px;
-            outline: none;
-            padding: 12px 15px;
-            font-size: 18px;
-            transition: all .1s linear;
-            -webkit-transition: all .1s linear;
-            -moz-transition: all .1s linear;
-            -webkit-appearance:none;
-            font-weight: 200;
-            background: #212529;
-        }
-
-        .label-float input:focus{
-            border: 2px solid #3951b2;
-        }
-
-        .label-float input::placeholder{
-            color:transparent;
-        }
-
-        .label-float label{
-            pointer-events: none;
-            position: absolute;
-            top: calc(50% - 8px);
-            left: 15px;
-            transition: all .1s linear;
-            -webkit-transition: all .1s linear;
-            -moz-transition: all .1s linear;
-            background-color: white;
-            padding: 3px;
-            box-sizing: border-box;
-            font-weight: 400;
-            background: #212529;
-        }
-
-        
-        .label-float input:focus + label,
-        .label-float input:not(:placeholder-shown) + label{
-            font-size: 15px;
-            top: 0;
-            color: #FFF;
-        }
-        .filled {
-            display: flex;
-        }
-        .input-longo input {
-            width: 100%;
-        }
-        .input-setenta {
-            width: 75%;
-        }
-        .input-vinte {
-            width: 25%;
-        }
-        .input-cinquenta {
-            width: 50%;
-        }
-        .input-trinta {
-            width: 33%;
-        }
-        #cep, #uf, #cidade, #bairro, #endereco , #num, #compl, #senha, #senha2{
-            width: 100%;
-        }
-        .fas, .fa-eye, .ver{
-            margin: 3px 7px;
-            background: #212529;
-        }
-        </style>
-    </head>
-
-    <body>
-        <main class="container">
-        <div id="header">
-            <div><h2>Cadastro</h2></div>
-            <div id="voltar">
-                <a href="{{ url('/') }}">VOLTAR</a>
-            </div>
+<body>
+    <div id="navbar">
+        <div id="nav-logo">
+            <a href="{{ url('/') }}"><img src="{{ url('/imgs/logo_simplify.png') }}" alt="Logo Simplify"></a>
         </div>
+
+        @if (Route::has('login'))
+        <div id="nav-link">
+            <a href="{{ url('/') }}">VOLTAR</a>
+        </div>
+        @endif
+
+        </div>
+    </div>
+    <div id="fundo">
+        <div id="container">
+            <div id="header">
+                <div><h2>Cadastro</h2></div>
+            </div>
             <form action="{{ url('/cadastro_empresa')}}" method="post"  enctype="multipart/form-data" data-viacep>
                 {{ csrf_field() }}
                 <div class="etapas">
@@ -237,24 +133,25 @@
                 </div>
             </form>
         </div>
+    </div>
 
-        <script>
-            $('#form').on( 'viacep.ajax.complete', function () {
-                // remover o header para esta requisicao
-                delete $.ajaxSettings.headers["X-CSRF-TOKEN"];
-                var $this = $( this ), fields_to_block = ['cep', 'rua', 'bairro', 'cidade', 'estado'];
+    <script>
+        $('#form').on( 'viacep.ajax.complete', function () {
+            // remover o header para esta requisicao
+            delete $.ajaxSettings.headers["X-CSRF-TOKEN"];
+            var $this = $( this ), fields_to_block = ['cep', 'rua', 'bairro', 'cidade', 'estado'];
 
-                fields_to_block.forEach(function (name) {
-                    $this.find('[name="' + name + '"]').attr('disabled', true);
-                });
-            }).on('viacep.ajax.complete', function () {
-                document.getElementById("#num").focus();
-                // readicionar o header para outras requisições internas ao projeto.
-                $.ajaxSettings.headers["X-CSRF-TOKEN"] = $('meta[name="csrf-token"]').attr('content');
+            fields_to_block.forEach(function (name) {
+                $this.find('[name="' + name + '"]').attr('disabled', true);
             });
-        </script>
-   
-    </body>
+        }).on('viacep.ajax.complete', function () {
+            document.getElementById("#num").focus();
+            // readicionar o header para outras requisições internas ao projeto.
+            $.ajaxSettings.headers["X-CSRF-TOKEN"] = $('meta[name="csrf-token"]').attr('content');
+        });
+    </script>
+
+</body>
 
 </html>
 
