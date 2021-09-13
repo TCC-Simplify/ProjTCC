08f028a63450d15b5a6d07825315ce4753bd25de
Fernanda Modolo <fernanda.modolo@unesp.br>
2021-09-13 15:54:12 -0300

    primeiras funcoes mural

--- app/Http/Controllers/MuralController.php
+++ app/Http/Controllers/MuralController.php
@@ -15,5 +15,38 @@ use Illuminate\Support\Facades\DB;
 
 class MuralController extends Controller
 {
-    //
+    public function criar_aviso(Request $request, User $user)
+    {
+        $id_user = Auth::user()->id;
+
+        Aviso::create([
+         'titulo' =>  $request['titulo'],
+         'descricao' =>  $request['descricao'],
+         'responsavel' =>  $id_user,
+         'imagem' =>  $request['imagem'],
+         'video' =>  $request['video'],
+         'duracao' =>  $request['duracao']
+
+        ] );
+
+        return redirect('/mural');
+    }
+
+    public function mostra_avisos()
+    {
+        $id_user = Auth::user()->id;
+        $avisos = Avisos::all()->where('responsavel',$id_user);
+        return view('mural/avisos', compact('avisos'));
+
+    }
+
+    public function criar_reuniao(Request $request, User $user)
+    {
+
+    }
+
+    public function mostra_reuniao()
+    {
+        
+    }
 }
